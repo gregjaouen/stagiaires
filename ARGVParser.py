554805b176ea8e201d9403d269e7561b5b033bbe
gregpaga <gregpaga@tutanota.com>
2020-01-02 14:35:29 +0100

    add ARGVParser

---
+++ ARGVParser.py
@@ -0,0 +1,46 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+
+class ARGVParser:
+
+    def __init__(self, pattern):
+        ## TODO: throw exception for minial argv
+        super().__init__()
+        self.pattern = pattern
+        self.rawArgs = sys.argv[1:]
+        self.__parseRaw()
+
+    def hasActions(self, *actions):
+        for index in range(len(actions)):
+            if actions[index] != self.parsed[index]:
+                return False
+        return True
+    
+    def getOptions(self):
+        return self.parsed[-1]
+
+    def __parseFromLevel(self, index, patternLevel):
+        if index < len(self.rawArgs[index]):
+            arg = self.rawArgs[index]
+            if type(patternLevel) == dict:
+                if arg in patternLevel:
+                    self.parsed.append(arg)
+                    self.__parseFromLevel(index+1, patternLevel[arg])
+                else:
+                    raise Exception("Unknown arg: {:s}".format(arg))
+            elif type(patternLevel) == list:
+                options = {}
+                for i in range(len(patternLevel)):
+                    options[patternLevel[i]] = self.rawArgs[index+i]
+                self.parsed.append(options)
+            else:
+                # TODO: throw Exception for unknown pattern type
+                pass
+
+    def __parseRaw(self):
+        self.parsed = []
+        self.__parseFromLevel(0, self.pattern)
+        print(self.parsed)
+
--- main.py
+++ main.py
@@ -4,26 +4,81 @@
 # TODO: Throw exceptions in users
 
 from User import User
+from ARGVParser import ARGVParser
 
-import sys
-import re
+# import sys
+# #utilisateur = input("Nom d'utilisateur:")
+# #mot_de_passe = input("Mot de passe:")
+# if len(sys.argv)<4:
+#     sys.exit('Usage: stagiaire.py identifiant motdepasse repository' )
 
-#utilisateur = input("Nom d'utilisateur:")
-#mot_de_passe = input("Mot de passe:")
-if len(sys.argv)<4:
-    sys.exit('Usage: stagiaire.py identifiant motdepasse repository' )
+# utilisateur = sys.argv[1]
+# mot_de_passe = sys.argv[2]
+# repository = sys.argv[3]
 
-utilisateur = sys.argv[1]
-mot_de_passe = sys.argv[2]
-repository = sys.argv[3]
+PATTERN = {
+    "new" : {
+        "user" : [
+            "username", "password"
+        ],
+        "repo" : [
+            "username", "reponame"
+        ]
+    },
+    "delete": {
+        "user": [
+            "username"
+        ],
+        "repo": [
+            "reponame"
+        ]
+    },
+    "edit": {
+        "user" : {
+            "username": ["username"],
+            "password": ["password"],
+        },
+        "repo" : {
+            "reponame": ["reponame"]
+        }
+    },
+    "list" : {
+        "user": [],
+        "repo": [
+            "username"
+        ] ,
+        "db" : [
+            "username"
+        ]
+    }
+}
 
-prog = re.compile("^[a-z][a-z0-9]{2,8}$")
-result = prog.match(utilisateur)
+p = ARGVParser(PATTERN)
+if p.hasActions("new", "user"):
+    options = p.getOptions()
+    user = User(options["username"], options["password"])
+elif p.hasActions("new", "repo"):
+    pass
+elif p.hasActions("delete", "user"):
+    pass
+elif p.hasActions("delete", "repo"):
+    pass
+elif p.hasActions("edit", "user"):
+    pass
+elif p.hasActions("edit", "user", "username"):
+    pass
+elif p.hasActions("edit", "user", "password"):
+    pass
+elif p.hasActions("edit", "repo", "reponame"):
+    pass
+elif p.hasActions("list", "user"):
+    pass
+elif p.hasActions("list", "repo"):
+    pass
+elif p.hasActions("list", "db"):
+    pass
 
-if result is None:
-    sys.exit("L'identifiant ne peut contenir que des lettres minuscules, des chiffres et ne peut pas dépasser 8 caratères  [a-z][a-z0-9]{2,7} ")
-
-user = User(utilisateur, mot_de_passe)
-user.create()
-user.createNewGitRepo(repository)
-user.createDBForGitRepo()
+# user = User(utilisateur, mot_de_passe)
+# user.create()
+# user.createNewGitRepo(repository)
+# user.createDBForGitRepo()
